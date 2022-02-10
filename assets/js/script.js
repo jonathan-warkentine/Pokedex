663eb57882f1c0012b5f8433e55816fe2d60558c
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-02-09 19:26:56 -0500

    introduced a form-submit event listener that fetches the submitted search to the pokemon API

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Search Submit Event Listener
+$("#form").on("submit", function(event){
+    event.preventDefault();
+    console.log("search submitted");
+    fetchPokemon(document.querySelector("#searchBar").value)
+})
+
+// Fetches Search from Pokemon API
+function fetchPokemon(search) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+    .then( function(response){
+        return response.json();
+    })
+    .then( function(data){
+        console.log(data);
+    })
+    .catch( function(error){
+        alert(error);
+    })
+}
+    
